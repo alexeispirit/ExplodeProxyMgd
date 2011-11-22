713296763313b8851e17ad1461163c0d4c808f09
alexeispirit <alexeispirit@gmail.com>
2011-11-22 12:11:21 +0600

    (proxy-explode-entity ent) renamed into (proxy-explode-to-block ent str); (proxy-explode-in-place ent) added

--- ExplodeProxyMgd/ExplodeProxy.cs
+++ ExplodeProxyMgd/ExplodeProxy.cs
@@ -10,53 +10,105 @@ namespace ExplodeProxyMgd
 {
     public class ExplodeProxy
     {
-        [LispFunction("proxy-explode-entity")]
-        public static TypedValue ProxyExplodeEntity(ResultBuffer rbArgs)
+        
+        /// <summary>
+        /// Explode proxy entity to blockreference
+        /// Return string name of blockreference in drawing database
+        /// </summary>
+        /// <param name="rbArgs">Lisp arguments: entity objectId, string BlockPrefix</param>
+        /// <returns>string BlockReference name</returns>
+        [LispFunction("proxy-explode-to-block")]
+        public static TypedValue ProxyExplodeToBlock(ResultBuffer rbArgs)
         {
             Document doc = Application.DocumentManager.MdiActiveDocument;
             Database db = doc.Database;
+
             TypedValue res = new TypedValue((int)LispDataType.Text,"");
-            if (rbArgs != null)
+
+            if (rbArgs.AsArray().Length == 2)
             {
-                foreach (TypedValue rb in rbArgs)
+                TypedValue entity = rbArgs.AsArray()[0];
+                TypedValue blkPrefix = rbArgs.AsArray()[1];
+                
+                if ((entity.TypeCode == (int)LispDataType.ObjectId) && (blkPrefix.TypeCode == (int)LispDataType.Text))
                 {
-                    if (rb.TypeCode == (int)LispDataType.ObjectId)
+                    using (Transaction tr = doc.TransactionManager.StartTransaction())
                     {
-                        using (Transaction tr = doc.TransactionManager.StartTransaction())
+                        ObjectId id = (ObjectId)entity.Value;
+                        DBObjectCollection objs = new DBObjectCollection();
+                        BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForRead);
+                        
+                        Entity entx = (Entity)tr.GetObject(id, OpenMode.ForWrite);
+                        entx.Explode(objs);
+                        
+                        string blkName = blkPrefix.Value.ToString() + entx.Handle.ToString();
+                        
+                        if (bt.Has(blkName) == false)
                         {
-                            ObjectId id = (ObjectId)rb.Value;
-                            DBObjectCollection objs = new DBObjectCollection();
-                            BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForRead);
-
-                            Entity entx = (Entity)tr.GetObject(id, OpenMode.ForWrite);
-                            entx.Explode(objs);
-
-                            string blkName = "ncs-acad-" + entx.Handle.ToString();
-
-                            if (bt.Has(blkName) == false)
+                            BlockTableRecord btr = new BlockTableRecord();
+                            btr.Name = blkName;
+                            
+                            bt.UpgradeOpen();
+                            ObjectId btrId = bt.Add(btr);
+                            tr.AddNewlyCreatedDBObject(btr, true);
+                            
+                            foreach (DBObject obj in objs)
                             {
-                                BlockTableRecord btr = new BlockTableRecord();
-                                btr.Name = blkName;
-
-                                bt.UpgradeOpen();
-                                ObjectId btrId = bt.Add(btr);
-                                tr.AddNewlyCreatedDBObject(btr, true);
-
-                                foreach (DBObject obj in objs)
-                                {
-                                    Entity ent = (Entity)obj;
-                                    btr.AppendEntity(ent);
-                                    tr.AddNewlyCreatedDBObject(ent, true);
-                                }
+                                Entity ent = (Entity)obj;
+                                btr.AppendEntity(ent);
+                                tr.AddNewlyCreatedDBObject(ent, true);
                             }
-                            res = new TypedValue((int)LispDataType.Text, blkName);
-
-                            tr.Commit();
                         }
+                        res = new TypedValue((int)LispDataType.Text, blkName);
+                        
+                        tr.Commit();
                     }
                 }
             }
             return res;
+        }
+
+        /// <summary>
+        /// Explode proxy entity in place
+        /// </summary>
+        /// <param name="rbArgs">entity name</param>
+        [LispFunction("proxy-explode-in-place")]
+        public static void ProxyExplodeInPlace(ResultBuffer rbArgs)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+
+            if (rbArgs.AsArray().Length == 1)
+            {
+                TypedValue entity = rbArgs.AsArray().First();
+
+                if (entity.TypeCode == (int)LispDataType.ObjectId)
+                {
+                    using (Transaction tr = doc.TransactionManager.StartTransaction())
+                    {
+                        ObjectId id = (ObjectId)entity.Value;
+                        DBObjectCollection objs = new DBObjectCollection();
+                        BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForRead);
+
+                        Entity entx = (Entity)tr.GetObject(id, OpenMode.ForWrite);
+                        entx.Explode(objs);
+
+                        entx.Erase();
+
+                        BlockTableRecord btr = (BlockTableRecord)tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite);
+
+                        foreach (DBObject obj in objs)
+                        {
+                            Entity ent = (Entity)obj;
+                            btr.AppendEntity(ent);
+                            tr.AddNewlyCreatedDBObject(ent, true);
+                        }
+
+                        tr.Commit();
+                    }
+
+                }
+            }
         }
     }
 }
